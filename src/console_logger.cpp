8ae5a0608e597c099ca3f91704886a3611ed0821
Clay Whitelytning <cwhitelytning@gmail.com>
2024-04-13 20:45:45 +0300

    Logging level flags were not taken into account in the console logger.

--- src/console_logger.cpp
+++ src/console_logger.cpp
@@ -1,15 +1,18 @@
 #include <mi/console_logger.hpp>
 #include <mi/datetime.hpp>
+#include <mi/bitflag.hpp>
 
 using namespace mi;
 
 void
 console_logger::log(const extension &sender, logger_level level, ustring_view message)
 {
-    interpolate_stream(uclog,
-                       USTRING("L {}\t[{}]\t{}\t{}"),
-                       now_datetime(USTRING("%Y.%m.%d %H:%M:%S")),
-                       sender.classname(),
-                       logger_level_to_string(level),
-                       message);
+    if (BITFLAG_CHECK_BY_INDEX(flags(), level)) {
+        interpolate_stream(uclog,
+                           USTRING("L {}\t[{}]\t{}\t{}"),
+                           now_datetime(USTRING("%Y.%m.%d %H:%M:%S")),
+                           sender.classname(),
+                           logger_level_to_string(level),
+                           message);
+    }
 }
